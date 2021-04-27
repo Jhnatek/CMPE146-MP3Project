780e550065b212a50e62765c0153511b4c5d5dc0
Jhnatek <42760223+Jhnatek@users.noreply.github.com>
2021-04-26 18:31:46 -0700

    w

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -16,20 +16,19 @@
 typedef char songname_t[16]; // not quite sure what the purpose of this is, im prettu sure its used in app_cli.c
 typedef char songbyte_t[512];
 static const uint32_t play_pause_pin = (1 << 19);
-TaskHandle_t MP3PlayPause =NULL;
+TaskHandle_t MP3PlayPause = NULL;
 void play_pause_button(void *p);
 void mp3_reader_task(void *p);
 void mp3_player_task(void *p);
 QueueHandle_t Q_songname;
 QueueHandle_t Q_songdata;
 
-
 // flash: python nxp-programmer/flash.py
 
 void main(void) {
   sj2_cli__init();
   mp3_decoder__initialize();
-  xTaskCreate(play_pause_button, "Play/Pause", (4096 / sizeof(void *)),, NULL, Priority_Medium, NULL);
+  xTaskCreate(play_pause_button, "Play/Pause", (4096 / sizeof(void *)), , NULL, Priority_Medium, NULL);
   xTaskCreate(mp3_reader_task, "read-task", (4096 / sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
   xTaskCreate(mp3_player_task, "play-task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   Q_songname = xQueueCreate(1, sizeof(songname_t));
