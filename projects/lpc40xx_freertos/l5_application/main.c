327acc2c74facc7d3a4323b3abe10ca8e400c921
Princej258 <74393262+Princej258@users.noreply.github.com>
2021-05-07 21:59:49 -0700

    fixed pause

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -16,10 +16,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #define SCI_VOL 0x0B
-#define volume_up gpio__construct_as_input(1, 15)   // josh changed from 1,10 to 1,15 which is sw1
-#define volume_down gpio__construct_as_input(0, 30) // josh changed from 1,14 to 0,30 for sw2
+#define volume_up gpio__construct_as_input(1, 10)
+#define volume_down gpio__construct_as_input(1, 14)
 #define play_pause gpio__construct_as_input(1, 19)
 
+xTaskHandle Player;
 // LPC_IOCON->P0_8 &= ~(3 << 3);
 // LPC_IOCON->P0_8 |= (1 << 3);
 typedef char songname_t[16]; // not quite sure what the purpose of this is, im prettu sure its used in app_cli.c
@@ -51,10 +52,10 @@ void main(void) {
   // gpio1__set_as_input(9);
   // gpio1__set_as_input(10);
   // gpio1__set_as_input(14);
-  // xTaskCreate(Play_Pause_Button, "Play/Pause", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
+  xTaskCreate(Play_Pause_Button, "Play/Pause", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   // xTaskCreate(Volume_Control, "Volume Control", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   xTaskCreate(mp3_reader_task, "read-task", (4096 / sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
-  xTaskCreate(mp3_player_task, "play-task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
+  xTaskCreate(mp3_player_task, "play-task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, &Player);
   Q_songname = xQueueCreate(1, sizeof(songname_t));
   Q_songdata = xQueueCreate(1, 512);
 
@@ -72,9 +73,11 @@ void mp3_reader_task(void *p) {
   while (true) {
     if (xQueueReceive(Q_songname, &name, portMAX_DELAY)) {
       file = f_open(&songFile, name, FA_READ);
+      fprintf(stderr, "file %d\n", file);
       if (FR_OK == file) {
         while (!f_eof(&songFile)) {
           f_read(&songFile, bytes_512, 512, &byte_reader);
+          fprintf(stderr, "reading file \n");
           xQueueSend(Q_songdata, &bytes_512, portMAX_DELAY);
         }
         f_close(&songFile);
@@ -88,68 +91,78 @@ void mp3_reader_task(void *p) {
 // Player task receives song data over Q_songdata to send it to the MP3 decoder
 void mp3_player_task(void *p) {
   char bytes_512[512];
-  bool pause = false;
-  // uint8_t alternative = 1;
 
   while (1) {
-    if (gpio__get(play_pause)) {
-      if (pause == true) {
-        pause == false;
-      } else if (pause == false) {
-        pause == true;
+  
+    xQueueReceive(Q_songdata, &bytes_512[0], portMAX_DELAY);
+    for (int i = 0; i < sizeof(bytes_512); i++) {
+      // fprintf(stderr, "%x", bytes_512[i]); used for testing milestone 
+    uint8_t alternate_status = 1;
+      while (!mp3_decoder__needs_data()) { // need to make this
+        fprintf(stderr, "%x", bytes_512[i]);
       }
+      // fprintf(stderr, "Sending to decoder:\n");
+      spi_send_to_mp3_decoder(bytes_512[i]); // need to make this
+      // fprintf(stderr, "sent\n");
+      
     }
-    if (pause == false) {
-      pause == gpio__get(play_pause);
-      xQueueReceive(Q_songdata, &bytes_512[0], portMAX_DELAY);
-      for (int i = 0; i < sizeof(bytes_512); i++) {
-        bool play_status = false;
-        uint8_t alternate_status = 1;
-        while (!mp3_decoder__needs_data()) {
-        }
-        spi_send_to_mp3_decoder(bytes_512[i]);
-      }
-    } else if (pause == true) {
-      fprintf(stderr, "MUSIC PAUSED!");
-    }
+  
   }
 } // josh added, double check its where you want
 
-// void Play_Pause_Button(void *p) {
-//   // gpio1__set_as_input(9);
-//   bool pause = false;
-//   while (1) {
-//     if (pause == true && gpio__get(play_pause)) {
-//       vTaskResume(mp3_player_task);
-//       pause = false;
-//     }
-//      if (pause == false && gpio__get(play_pause)) {
-//         vTaskSuspend;
-//         pause = true;
-//       }
+void Play_Pause_Button(void *p) {
+  // gpio1__set_as_input(9);
+  bool pause = false;
+  bool previous = false;
+  while (1) {
+    if (gpio__get(play_pause) && !previous) {
+      pause = true;
+    }
+    if (gpio__get(play_pause) && previous) {
+      pause = false;
+    }
+    if (!gpio__get(play_pause)){
+      previous = pause;
+    }
+    if (pause) {
+      vTaskSuspend(Player);
+      fprintf(stderr, "MUSIC PAUSED!");
+    }
+    if (!pause) {
+      vTaskResume(Player);
+    }
+    
+    // if (pause == true && gpio__get(play_pause)) {
+    //   vTaskResume(mp3_player_task);
+    //   pause = false;
+    // }
+    //  if (pause == false && gpio__get(play_pause)) {
+    //     vTaskSuspend;
+    //     pause = true;
+    //   } 
 
-//     // while (1) {
-//     //   if (gpio__get(play_pause)) {
-//     //     play_status = true;
-//     //   }
+    // while (1) {
+    //   if (gpio__get(play_pause)) {
+    //     play_status = true;
+    //   } 
 
-//     //   if (play_status) {
-//     //     vTaskSuspend(mp3_player_task);
-//     //     while (1) {
-
-//     //     }
-//     //     if (alternate_status) {
-//     //       vTaskResume(mp3_player_task);
-//     //       alternate_status--;
-//     //     } else {
-//     //       vTaskSuspend(mp3_player_task);
-//     //       alternate_status++;
-//     //     }
-//     //   }
-//     //   vTaskDelay(1);
-//     // }
-//   }
-// }
+    //   if (play_status) {
+    //     vTaskSuspend(mp3_player_task);
+    //     while (1) {
+          
+    //     }
+    //     if (alternate_status) {
+    //       vTaskResume(mp3_player_task);
+    //       alternate_status--;
+    //     } else {
+    //       vTaskSuspend(mp3_player_task);
+    //       alternate_status++;
+    //     }
+    //   }
+    //   vTaskDelay(1);
+    // }
+  }
+}
 
 void Volume_Control(void *p) {
   // gpio_s volume_up = gpio__construct_as_input(1, 10);
