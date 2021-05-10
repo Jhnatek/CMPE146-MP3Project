6ab8a59db25bad7dac2b5ef3d96557ce2dea1d8d
43nreddy <37527973+43nreddy@users.noreply.github.com>
2021-05-09 21:32:18 -0700

    Update main.c

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,77 +1,78 @@
+#include <stdio.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+#include "LPC40xx.h"
+#include "app_cli.h"
 #include "board_io.h"
 #include "common_macros.h"
+#include "decoder.h"
+#include "ff.h"
+#include "lcd_driver.h"
 #include "periodic_scheduler.h"
-#include "queue.h"
+#include "semphr.h"
 #include "sj2_cli.h"
 
-#include "uart_lab.h"
-// python nxp-programmer/flash.py
+typedef char songname_t[16]; // not quite sure what the purpose of this is, im prettu sure its used in app_cli.c
+void mp3_reader_task(void *p);
+void mp3_player_task(void *p);
+QueueHandle_t Q_songname;
+QueueHandle_t Q_songdata;
 
-void uart_read_task(void *p) {
-  char *test;
-  while (1) {
-    if (uart_lab__get_char_from_queue(&test, portMAX_DELAY)) { // took out & from test
-      fprintf(stderr, "Test back: %c\n ", test);               // port 28
-      vTaskDelay(500);
-    } else {
-      fprintf(stderr, "error in read task\n");
-    }
-  }
-}
-
-void uart_write_task(void *p) {
-  // char test[5] = "test-"; // dont use double quotes!!!
-  char test = 't';
-  while (1) {
-    for (int i = 0; i < 5; i++) {
-      uart_lab__polled_put(UART_2, test); // port 29
-      printf("Test sent: %c\n ", test);
-    }
-    vTaskDelay(500);
-  }
-}
+// flash: python nxp-programmer/flash.py
 
 void main(void) {
-  uart_lab__init(UART_2, 96, 38400);
-  uart__enable_receive_interrupt(UART_2);
-  xTaskCreate(uart_read_task, "test1", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-  xTaskCreate(uart_write_task, "test2", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+  sj2_cli__init();
+  mp3_decoder__initialize();
+  lcd__initialize();
+  xTaskCreate(mp3_reader_task, "read-task", (4096 / sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
+  xTaskCreate(mp3_player_task, "play-task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
+  Q_songname = xQueueCreate(1, sizeof(songname_t));
+  Q_songdata = xQueueCreate(1, 512);
 
   vTaskStartScheduler();
 }
 
-#if (0)
-part 1 void uart_read_task(void *p) {
-  while (1) {
-    char *test;
-    uart_lab__polled_get(UART_2, &test); // port 28
-    printf("Test back: %c\n ", test);
-    vTaskDelay(500);
+// Reader tasks receives song-name over Q_songname to start reading it
+void mp3_reader_task(void *p) {
+  songname_t name;
+  char bytes_512[512];
+  UINT *byte_reader;
+  FRESULT file;
+  const char *song_pointer = name; // maybe delete
+  FIL songFile;
+  while (true) {
+    if (xQueueReceive(Q_songname, &name, portMAX_DELAY)) {
+      file = f_open(&songFile, name, FA_READ);
+      fprintf(stderr, "file %d\n", file);
+      if (FR_OK == file) {
+        while (!f_eof(&songFile)) {
+          f_read(&songFile, bytes_512, 512, &byte_reader);
+          fprintf(stderr, "reading file \n");
+          xQueueSend(Q_songdata, &bytes_512, portMAX_DELAY);
+        }
+        f_close(&songFile);
+      } else {
+        fprintf(stderr, "Failed to open file \n");
+      }
+    }
   }
 }
 
-void uart_write_task(void *p) {
+// Player task receives song data over Q_songdata to send it to the MP3 decoder
+void mp3_player_task(void *p) {
+  char bytes_512[512];
+
   while (1) {
-    char test = 't';                    // dont use double quotes!!!
-    uart_lab__polled_put(UART_2, test); // port 29
-    printf("Test sent: %c\n ", test);
-    vTaskDelay(500);
+    xQueueReceive(Q_songdata, &bytes_512[0], portMAX_DELAY);
+    for (int i = 0; i < sizeof(bytes_512); i++) {
+      while (!mp3_decoder__needs_data()) { // need to make this
+        fprintf(stderr, "%x", bytes_512[i]);
+      }
+      fprintf(stderr, "Sending to decoder:\n");
+      spi_send_to_mp3_decoder(bytes_512[i]); // need to make this
+      fprintf(stderr, "sent\n");
+    }
   }
-}
-
-void main(void) {
-  // TODO: Use uart_lab__init() function and initialize UART2 or UART3 (your choice)
-  // TODO: Pin Configure IO pins to perform UART2/UART3 function
-  uart_lab__init(UART_2, 96, 38400);
-  xTaskCreate(uart_read_task, "test1", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-  xTaskCreate(uart_write_task, "test2", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-
-  vTaskStartScheduler();
-}
-#endif
+} // josh added, double check its where you want
