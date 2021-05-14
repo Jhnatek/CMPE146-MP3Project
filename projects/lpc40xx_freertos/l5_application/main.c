ac76e224e2eef5ad9bb1f3649bb1b1d526e66659
Jhnatek <42760223+Jhnatek@users.noreply.github.com>
2021-05-13 22:42:22 -0700

    still fixing matrix

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -5,28 +5,14 @@
 
 #include "board_io.h"
 #include "common_macros.h"
+#include "matrix.c"
 #include "periodic_scheduler.h"
 #include "sj2_cli.h"
-#include "matrix.c"
 // flash: python nxp-programmer/flash.py
 
 int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
-
-  // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
-  // See esp32/README.md for more details
-  // uart3_init();                                                                     // Also include:  uart3_init.h
-  // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
-
-  puts("Starting RTOS,  why hello there world");
-  // LAB 2
-
-  xTaskCreate(task_one, "aaaaaa", 1000, NULL, PRIORITY_LOW, NULL);
-  xTaskCreate(task_two, "bbbbbb", 1000, NULL, PRIORITY_HIGH, NULL);
-
-  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
-
+  while () {
+    scan_matrix();
+  }
   return 0;
 }
-
--- projects/lpc40xx_freertos/l5_application/matrix.c
+++ projects/lpc40xx_freertos/l5_application/matrix.c
@@ -15,8 +15,8 @@ For an Arduino Mega 2560
 This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 
 Distributed as-is; no warranty is given.
-******************************************************************************/    
-//config variables
+******************************************************************************/
+// config variables
 #include <stdio.h>
 
 #include "FreeRTOS.h"
@@ -26,87 +26,98 @@ Distributed as-is; no warranty is given.
 #include "common_macros.h"
 #include "periodic_scheduler.h"
 #include "sj2_cli.h"
-#define NUM_LED_COLUMNS (4)
+#define NUM_LED_COLUMNS (1)
 #define NUM_LED_ROWS (4)
-#define NUM_BTN_COLUMNS (4)
+#define NUM_BTN_COLUMNS (1)
 #define NUM_BTN_ROWS (4)
 #define NUM_COLORS (1)
 #define MAX_DEBOUNCE (3)
-//2.0,2.2,2.5,2.7,2.9
-//2.1,2.4,2.6,2.8,0.17
+#define btn_column_1 gpio__construct(2, 1)
+#define btn_column_2 gpio__construct(2, 4)
+#define btn_column_3 gpio__construct(2, 6)
+#define btn_column_4 gpio__construct(2, 8)
+#define btn_row_1 gpio__construct(0, 17)
+#define LED_column_1 gpio__construct(2, 0)
+#define LED_column_2 gpio__construct(2, 2)
+#define LED_column_3 gpio__construct(2, 5)
+#define LED_column_4 gpio__construct(2, 7)
+#define LED_row_1 gpio__construct(2, 9)
+// 2.0,2.2,2.5,2.7,2.9
+// 2.1,2.4,2.6,2.8,0.17
 // Global variables
 static bool LED_buffer[NUM_LED_COLUMNS][NUM_LED_ROWS];
 
-static const gpio_s btncolumnpins[NUM_BTN_COLUMNS] = {50, 51, 52, 53};
-static const gpio_s btnrowpins[NUM_BTN_ROWS]       = {46};
-static const gpio_s ledcolumnpins[NUM_LED_COLUMNS] = {42, 43, 44, 45};
-static const gpio_s colorpins[NUM_LED_ROWS]        = {22};
-
+static const gpio_s btncolumnpins[NUM_BTN_COLUMNS] = {btn_column_1, btn_column_2, btn_column_3, btn_column_4};
+static const gpio_s btnrowpins[NUM_BTN_ROWS] = {btn_row_1};
+static const gpio_s ledcolumnpins[NUM_LED_COLUMNS] = {LED_column_1, LED_column_2, LED_column_3, LED_column_4};
+static const gpio_s colorpins[NUM_LED_ROWS] = {LED_row_1};
 static int8_t debounce_count[NUM_BTN_COLUMNS][NUM_BTN_ROWS];
 
-static void setuppins()
-{
+static void setuppins() {
   uint8_t i;
 
   // initialize
   // select lines
   // LED columns
-  for (i = 0; i < NUM_LED_COLUMNS; i++)
-  {
-    pinMode(ledcolumnpins[i], OUTPUT);
-
+  for (i = 0; i < NUM_LED_COLUMNS; i++) {
+    // arduino:
+    // pinMode(ledcolumnpins[i], OUTPUT);
+    // sj2:
+    gpio__set_as_output(ledcolumnpins[i]);
     // with nothing selected by default
-    digitalWrite(ledcolumnpins[i], HIGH);
+    // arduino:
+    // digitalWrite(ledcolumnpins[i], HIGH);
+    // sj2:
+    gpio__set(ledcolumnpins[i]);
   }
 
   // button columns
-  for (i = 0; i < NUM_BTN_COLUMNS; i++)
-  {
-    pinMode(btncolumnpins[i], OUTPUT);
-
-    // with nothing selected by default
-    digitalWrite(btncolumnpins[i], HIGH);
+  for (i = 0; i < NUM_BTN_COLUMNS; i++) {
+    gpio__set_as_output(btncolumnpins[i]);
+    gpio__set(btncolumnpins[i]);
   }
 
   // button row input lines
-  for (i = 0; i < NUM_BTN_ROWS; i++)
-  {
-    pinMode(btnrowpins[i], INPUT_PULLUP);
+  for (i = 0; i < NUM_BTN_ROWS; i++) {
+    // arduino:
+    // pinMode(btnrowpins[i], INPUT_PULLUP); We dont really have pullup... so i guess just an ordinarry input
+    // sj2:
+    gpio__set_as_input(btnrowpins[i]);
   }
 
   // LED drive lines
-  for (i = 0; i < NUM_LED_ROWS; i++)
-  {
-    pinMode(colorpins[i], OUTPUT);
-    digitalWrite(colorpins[i], LOW);
+  for (i = 0; i < NUM_LED_ROWS; i++) {
+    // arduino:
+    // pinMode(colorpins[i], OUTPUT);
+    // digitalWrite(colorpins[i], LOW);
+    gpio__set_as_output(colorpins[i]);
+    gpio__set(colorpins[i]);
   }
 
   // Initialize the debounce counter array
-  for (uint8_t i = 0; i < NUM_BTN_COLUMNS; i++)
-  {
-    for (uint8_t j = 0; j < NUM_BTN_ROWS; j++)
-    {
+  for (uint8_t i = 0; i < NUM_BTN_COLUMNS; i++) {
+    for (uint8_t j = 0; j < NUM_BTN_ROWS; j++) {
       debounce_count[i][j] = 0;
     }
   }
 }
 
-void scan()
-{
+void scan_matrix() {
   static uint8_t current = 0;
   uint8_t val;
   uint8_t i, j;
 
-  // Select current columns
-  digitalWrite(btncolumnpins[current], LOW);
-  digitalWrite(ledcolumnpins[current], LOW);
+  // Select current columns arduino:
+  // digitalWrite(btncolumnpins[current], LOW);
+  // digitalWrite(ledcolumnpins[current], LOW);
+  gpio__reset(btncolumnpins[current]);
+  gpio__reset(ledcolumnpins[current]);
 
   // output LED row values
-  for (i = 0; i < NUM_LED_ROWS; i++)
-  {
-    if (LED_buffer[current][i])
-    {
-      digitalWrite(colorpins[i], HIGH);
+  for (i = 0; i < NUM_LED_ROWS; i++) {
+    if (LED_buffer[current][i]) {
+      gpio__set(colorpins[i]);
+      // digitalWrite(colorpins[i], HIGH);
     }
   }
 
@@ -114,89 +125,72 @@ void scan()
   delay(1);
 
   // Read the button inputs
-  for ( j = 0; j < NUM_BTN_ROWS; j++)
-  {
-    val = digitalRead(btnrowpins[j]);
+  for (j = 0; j < NUM_BTN_ROWS; j++) {
+    // arduino:
+    // val = digitalRead(btnrowpins[j]);
+    val = gpio__get(btnrowpins[j]);
 
-    if (val == LOW)
+    if (val == 0) // arduino equivalent is LOW
     {
       // active low: val is low when btn is pressed
-      if ( debounce_count[current][j] < MAX_DEBOUNCE)
-      {
+      if (debounce_count[current][j] < MAX_DEBOUNCE) {
         debounce_count[current][j]++;
-        if ( debounce_count[current][j] == MAX_DEBOUNCE )
-        {
-          Serial.print("Key Down ");
-          Serial.println((current * NUM_BTN_ROWS) + j);
+        if (debounce_count[current][j] == MAX_DEBOUNCE) {
+          fprintf(stderr, "Key Down \n");
+          // Serial.println((current * NUM_BTN_ROWS) + j); //not sure what equivalent is in sj2
 
           // Do whatever you want to with the button press here:
           // toggle the current LED state
           LED_buffer[current][j] = !LED_buffer[current][j];
         }
       }
-    }
-    else
-    {
+    } else {
       // otherwise, button is released
-      if ( debounce_count[current][j] > 0)
-      {
+      if (debounce_count[current][j] > 0) {
         debounce_count[current][j]--;
-        if ( debounce_count[current][j] == 0 )
-        {
-          Serial.print("Key Up ");
-          Serial.println((current * NUM_BTN_ROWS) + j);
+        if (debounce_count[current][j] == 0) {
+          fprintf(stderr, "Key Up \n");
+          // Serial.println((current * NUM_BTN_ROWS) + j); //not sure what equivalent is
 
           // If you want to do something when a key is released, do it here:
-
         }
       }
     }
-  }// for j = 0 to 3;
+  } // for j = 0 to 3;
 
   delay(1);
+  // arduino
+  // digitalWrite(btncolumnpins[current], HIGH);
+  // digitalWrite(ledcolumnpins[current], HIGH);
+  // sj2
+  gpio__set(btncolumnpins[current]);
+  gpio__set(ledcolumnpins[current]);
 
-  digitalWrite(btncolumnpins[current], HIGH);
-  digitalWrite(ledcolumnpins[current], HIGH);
-
-  for (i = 0; i < NUM_LED_ROWS; i++)
-  {
-    digitalWrite(colorpins[i], LOW);
+  for (i = 0; i < NUM_LED_ROWS; i++) {
+    // sj2:
+    gpio_reset(colorpins[i]);
+    // digitalWrite(colorpins[i], LOW);
   }
 
   current++;
-  if (current >= NUM_LED_COLUMNS)
-  {
+  if (current >= NUM_LED_COLUMNS) {
     current = 0;
   }
-
 }
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-
-  Serial.print("Starting Setup...");
+  fprintf(stderr, "Starting Setup...\n");
 
   // setup hardware
   setuppins();
 
   // init global variables
-  for (uint8_t i = 0; i < NUM_LED_COLUMNS; i++)
-  {
-    for (uint8_t j = 0; j < NUM_LED_ROWS; j++)
-    {
+  for (uint8_t i = 0; i < NUM_LED_COLUMNS; i++) {
+    for (uint8_t j = 0; j < NUM_LED_ROWS; j++) {
       LED_buffer[i][j] = 0;
     }
   }
 
-  Serial.println("Setup Complete.");
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  scan();
-
+  fprintf(stderr, "Setup Complete.\n");
 }
