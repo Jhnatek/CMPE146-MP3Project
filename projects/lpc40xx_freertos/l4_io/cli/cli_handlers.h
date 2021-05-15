8fe0db1d6a3340aab5666fe3d293411b7c50c420
Jhnatek <42760223+Jhnatek@users.noreply.github.com>
2021-05-14 22:27:53 -0700

    for some reason im getting an error

--- projects/lpc40xx_freertos/l4_io/cli/cli_handlers.h
+++ projects/lpc40xx_freertos/l4_io/cli/cli_handlers.h
@@ -12,7 +12,7 @@ app_cli_status_e cli__i2c(app_cli__argument_t argument, sl_string_t user_input_m
 app_cli_status_e cli__task_list(app_cli__argument_t argument, sl_string_t user_input_minus_command_name,
                                 app_cli__print_string_function cli_output);
 
-app_cli_status_e cli__mp3_play(app_cli__argument_t argument, sl_string_t user_input_minus_command_name,
-                               app_cli__print_string_function cli_output);
+// app_cli_status_e cli__mp3_play(app_cli__argument_t argument, sl_string_t user_input_minus_command_name,
+//                              app_cli__print_string_function cli_output);
 // link for future additions:
 // http://books.socialledge.com/books/embedded-drivers-real-time-operating-systems/page/sj2-board
