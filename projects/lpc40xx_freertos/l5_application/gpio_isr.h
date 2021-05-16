205f785d36fc45082fb89a30534110766204a17b
43nreddy <37527973+43nreddy@users.noreply.github.com>
2021-05-16 15:21:20 -0700

    Update gpio_isr.h

--- projects/lpc40xx_freertos/l5_application/gpio_isr.h
+++ projects/lpc40xx_freertos/l5_application/gpio_isr.h
@@ -10,8 +10,8 @@ typedef enum {
 typedef void (*function_pointer_t)(void);
 
 // Allow the user to attach their callbacks
-void gpio0____attach_interrupt(uint32_t pin, gpio_interrupt_e interrupt_type, function_pointer_t callback);
-
+void gpio0____attach_interrupt(uint32_t pin, gpio_interrupt_e interrupt_type, function_pointer_t callback);// // was previously gpio0__attach_interrupt (uint32_t pin, gpio_interrupt_e interrupt_type, function_pointer_t callback)
+// changed function name to gpio0____attach_interrupt(uint32_t pin, gpio_interrupt_e interrupt_type, function_pointer_t callback) bc had the same function in interrupts.c 
 // Our main() should configure interrupts to invoke this dispatcher where we will invoke user attached callbacks
 // You can hijack 'interrupt_vector_table.c' or use API at lpc_peripherals.h
 void gpio0__interrupt_dispatcher(void);
