e22257c16bc9b701c78d2b4da6e2ef8d8c3999f1
43nreddy <37527973+43nreddy@users.noreply.github.com>
2021-05-16 10:36:49 -0700

    Update song_list.h

--- projects/lpc40xx_freertos/l5_application/song_list.h
+++ projects/lpc40xx_freertos/l5_application/song_list.h
@@ -1,8 +1,15 @@
 #pragma once
-
 #include <stddef.h> // size_t
+#include <stdint.h>
+#include <stdio.h>
 
 typedef char song_memory_t[128];
+
+/*
+ * [0] = filename, [1] = songname, [2] = artist, [3] = album, [4] = genre, [5] = year
+ */
+#define number_of_data 6
+typedef song_memory_t song_data_t[number_of_data];
 
 /* Do not declare variables in a header file */
 #if 0
@@ -10,6 +17,6 @@ static song_memory_t list_of_songs[32];
 static size_t number_of_songs;
 #endif
 
-void song_list__populate(void);
-size_t song_list__get_item_count(void);
-const char *song_list__get_name_for_item(size_t item_number);
+void MP3_song__init(void); // Populating Songs
+
+uint8_t MP3_song__get_overall_size(void);
