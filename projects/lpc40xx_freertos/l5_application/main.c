eb6f24293faa1e1895a633dbebcaa3dbff845614
Princej258 <74393262+Princej258@users.noreply.github.com>
2021-05-17 03:53:29 -0700

    Update main.c

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-
 #include "FreeRTOS.h"
-#include "task.h"
-
 #include "LPC40xx.h"
 #include "app_cli.h"
 #include "board_io.h"
@@ -15,11 +11,13 @@
 #include "periodic_scheduler.h"
 #include "semphr.h"
 #include "sj2_cli.h"
+#include "task.h"
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+/*  DEFINITIONS   */
 #define VOLUME 0x0B
 #define BASS 0x02
-
 #define play_pause gpio__construct_as_input(2, 2)
 #define bassincrease gpio__construct_as_input(2, 6)
 #define bassdecrease gpio__construct_as_input(2, 5)
@@ -34,37 +32,33 @@ void pull_down_switches(void);
 xTaskHandle Player;
 typedef char songname_t[16]; // not quite sure what the purpose of this is, im pretty sure naveen worked on this
 typedef char songbyte_t[512];
+/*  BUTTONS/SWITCHES   */
 void Play_Pause_Button(void *p);
 void volumedecrease_isr(void);
 void volumeincrease_isr(void);
+/*  TASKS   */
 void mp3_reader_task(void *p);
 void mp3_player_task(void *p);
-void volumeControl(bool higher, bool init);
 void volumeincrease_task(void *p);
 void volumedecrease_task(void *p);
-// void bassdecrease_isr(void);
-// void bassincrease_isr(void);
-// void trebledecrease_isr(void);
-// void trebleincrease_isr(void);
-void bass_function(bool higher, bool init);
-void treble_function(bool higher, bool init);
-void write_to_decoder_function(void);
 void bass_task(void *p);
 void treble_task(void *p);
+/*  WRITER/REQUIRED FUNCTIONS   */
+void volumeControl(bool higher);
+void bass_function(bool higher);
+void treble_function(bool higher);
+void write_to_decoder_function(void);
+
 QueueHandle_t Q_songname;
 QueueHandle_t Q_songdata;
 SemaphoreHandle_t Decoder_Mutex;
 SemaphoreHandle_t volumeincrease_semaphore;
 SemaphoreHandle_t volumedecrease_semaphore;
-// SemaphoreHandle_t bassdecrease_semaphore;
-// SemaphoreHandle_t bassincrease_semaphore;
-// SemaphoreHandle_t trebledecrease_semaphore;
-// SemaphoreHandle_t trebleincrease_semaphore;
 
+/*   DECLARATIONS/INITIALIZERS   */
 uint8_t volume_level = 5;
 uint8_t bass_level = 0;
 uint8_t treble_level = 0;
-// MP3_decoder__sci_write(VOLUME, 0x3030)
 
 // flash: python nxp-programmer/flash.py
 
@@ -72,30 +66,17 @@ void main(void) {
   pull_down_switches();
   volumedecrease_semaphore = xSemaphoreCreateBinary();
   volumeincrease_semaphore = xSemaphoreCreateBinary();
-  // bassincrease_semaphore = xSemaphoreCreateBinary();
-  // bassdecrease_semaphore = xSemaphoreCreateBinary();
-  // trebleincrease_semaphore = xSemaphoreCreateBinary();
-  // trebledecrease_semaphore = xSemaphoreCreateBinary();
   lpc_peripheral__enable_interrupt(LPC_PERIPHERAL__GPIO, gpio__interrupt_dispatcher, NULL);
   gpio__attach_interrupt(2, 8, GPIO_INTR__FALLING_EDGE, volumeincrease_isr);
   gpio__attach_interrupt(0, 17, GPIO_INTR__FALLING_EDGE, volumedecrease_isr);
-  // gpio__attach_interrupt(2, 2, GPIO_INTR__FALLING_EDGE, bassincrease_isr);
-  // gpio__attach_interrupt(2, 5, GPIO_INTR__FALLING_EDGE, bassdecrease_isr);
-  // gpio__attach_interrupt(2, 7, GPIO_INTR__FALLING_EDGE, trebleincrease_isr);
-  // gpio__attach_interrupt(2, 9, GPIO_INTR__FALLING_EDGE, trebledecrease_isr);
-  // gpio__attach_interrupt(0, 30, GPIO_INTR__FALLING_EDGE, bassincrease_isr);
-  // gpio__attach_interrupt(0, 29, GPIO_INTR__FALLING_EDGE, bassdecrease_isr);
-  // gpio__attach_interrupt(2, 7, GPIO_INTR__FALLING_EDGE, trebleincrease_isr);
-  // gpio__attach_interrupt(2, 9, GPIO_INTR__FALLING_EDGE, trebledecrease_isr);
-  // LPC_GPIO0->DIR &= ~(1 << 30);
   NVIC_EnableIRQ(GPIO_IRQn);
+
+  /*   TASKS WITH SCHEDULER    */
   sj2_cli__init();
   mp3_decoder__initialize();
-  xTaskCreate(bass_task, "bass task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
-  xTaskCreate(treble_task, "treble task", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
-
+  xTaskCreate(bass_task, "bass increase", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
+  xTaskCreate(treble_task, "treble increase", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   xTaskCreate(Play_Pause_Button, "Play/Pause", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
-  // xTaskCreate(Volume_Control, "Volume Control", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   xTaskCreate(volumeincrease_task, "volumeincrease", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   xTaskCreate(volumedecrease_task, "volumedecrease", (4096 / sizeof(void *)), NULL, PRIORITY_MEDIUM, NULL);
   xTaskCreate(mp3_reader_task, "read-task", (4096 / sizeof(void *)), NULL, PRIORITY_HIGH, NULL);
@@ -103,10 +84,11 @@ void main(void) {
   Q_songname = xQueueCreate(1, sizeof(songname_t));
   Decoder_Mutex = xSemaphoreCreateMutex();
   Q_songdata = xQueueCreate(1, 512);
-  // gpio__attach_interrupt(0, 29, GPIO_INTR__FALLING_EDGE, volumeincrease_isr);
 
   vTaskStartScheduler();
 }
+
+/*         FUNCTION DEFINITIONS           */
 
 // Reader tasks receives song-name over Q_songname to start reading it
 void mp3_reader_task(void *p) {
@@ -178,84 +160,70 @@ void Play_Pause_Button(void *p) {
   }
 }
 
-// void Volume_Control(void *p) {
-//   // Volume up pin: 10 port 1
-//   // Volume down pin: 14 port 1
-//   bool increase = false;
-//   bool decrease = false;
-//   while (1) {
-//     vTaskDelay(10);
-//     if (gpio__get(volume_up)) {
-//       while (gpio__get(volume_up)) {
-//       }
-//       increase = true;
-//     } else if (gpio__get(volume_down)) {
-//       while (gpio__get(volume_down)) {
-//       }
-//       decrease = true;
-//     } else {
-//       if (!gpio__get(volume_up)) {
-//         increase = false;
-//       } else if (!gpio__get(volume_down)) {
-//         decrease = false;
-//       }
-//     }
-
-//     if (increase) {
-//       volumeControl(true, false);
-//       increase = false;
-//     } else if (decrease) {
-//       volumeControl(false, false);
-//       decrease = false;
-//     }
-//   }
-// }
-
-void volumeControl(bool higher, bool init) {
+void volumeControl(bool higher) {
 
   fprintf(stderr, "GOT INTO VOLUMECONTROL\n");
-  if (higher && volume_level < 8 && !init) {
+  if (higher && volume_level < 16) {
     volume_level++;
-  } else if (!higher && volume_level > 1 && !init) {
+  } else if (!higher && volume_level > 0) {
     volume_level--;
   }
   fprintf(stderr, "GOT PAST IF CONDITIONS\n");
   if (xSemaphoreTake(Decoder_Mutex, portMAX_DELAY)) {
     switch (volume_level) {
-    case 8:
+    case 16:
+      MP3_decoder__sci_write(VOLUME, 0x0000);
+    case 15:
       MP3_decoder__sci_write(VOLUME, 0x1010);
-      // fprintf("volume: %i", volume_level);
+      break;
+    case 14:
+      MP3_decoder__sci_write(VOLUME, 0x2020);
+      break;
+    case 13:
+      MP3_decoder__sci_write(VOLUME, 0x3030);
+      break;
+    case 12:
+      MP3_decoder__sci_write(VOLUME, 0x4040);
+      break;
+    case 11:
+      MP3_decoder__sci_write(VOLUME, 0x5050);
+      break;
+    case 10:
+      MP3_decoder__sci_write(VOLUME, 0x6060);
+      break;
+    case 9:
+      MP3_decoder__sci_write(VOLUME, 0x7070);
+      break;
+    case 8:
+      MP3_decoder__sci_write(VOLUME, 0x8080);
       break;
     case 7:
-      MP3_decoder__sci_write(VOLUME, 0x2020);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0x9090);
       break;
     case 6:
-      MP3_decoder__sci_write(VOLUME, 0x2525);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0xA0A0);
       break;
     case 5:
-      MP3_decoder__sci_write(VOLUME, 0x3030);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0xB0B0);
       break;
     case 4:
-      MP3_decoder__sci_write(VOLUME, 0x3535);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0xC0C0);
       break;
     case 3:
-      MP3_decoder__sci_write(VOLUME, 0x3030);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0xD0D0);
       break;
     case 2:
-      MP3_decoder__sci_write(VOLUME, 0x4545);
-      // fprintf("volume: %i", volume_level);
+      MP3_decoder__sci_write(VOLUME, 0xE0E0);
       break;
     case 1:
+      MP3_decoder__sci_write(VOLUME, 0xF0F0);
+      break;
+    case 0:
       MP3_decoder__sci_write(VOLUME, 0xFEFE);
       // fprintf("volume: %i", volume_level);
       break;
     default:
-      MP3_decoder__sci_write(VOLUME, 0x3535);
+      MP3_decoder__sci_write(VOLUME, 0x5050);
       // fprintf("volume: %i", volume_level);
     }
     fprintf(stderr, "GOT PAST SWITCHES\n");
@@ -272,7 +240,7 @@ void volumeincrease_task(void *p) {
     if (xSemaphoreTake(volumeincrease_semaphore, portMAX_DELAY)) {
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      volumeControl(true, false);
+      volumeControl(true);
       vTaskDelay(10);
     }
   }
@@ -286,7 +254,7 @@ void volumedecrease_task(void *p) {
     if (xSemaphoreTake(volumedecrease_semaphore, portMAX_DELAY)) {
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      volumeControl(false, false);
+      volumeControl(false);
       // break;
       vTaskDelay(10);
     }
@@ -294,33 +262,27 @@ void volumedecrease_task(void *p) {
   xSemaphoreGive(volumedecrease_semaphore);
   vTaskDelay(250);
 }
-// void gpio_interrupt(void) {
-//   fprintf(stderr, "Interrupt has been received!!"); // prints that interrupt has been detected
-//   gpio__interrupt_dispatcher();                     // locates interrupt pin
-//   xSemaphoreGiveFromISR(volumedecrease_semaphore, NULL);
-//   volumedecrease_task;
-//   LPC_GPIOINT->IO0IntClr |= (1 << 30);
-// }
 
 void pull_down_switches(void) {
   gpio__enable_pull_down_resistors(play_pause); // Josh needs this because the buttons are active high
 }
 
-void bass_function(bool higher, bool init) {
+void bass_function(bool higher) {
   fprintf(stderr, "GOT INTO BASS FUNCTION\n");
-  if (higher && bass_level < 5 && !init) {
+  if (higher && bass_level < 5) {
+
     bass_level++;
-  } else if (!higher && bass_level > 1 && !init) {
+  } else if (!higher && bass_level > 1) {
     bass_level--;
   }
   write_to_decoder_function();
 }
 
-void treble_function(bool higher, bool init) {
+void treble_function(bool higher) {
   fprintf(stderr, "GOT INTO TREBLE FUNCTION\n");
-  if (higher && treble_level < 7 && !init) {
+  if (higher && treble_level < 7) {
     treble_level++;
-  } else if (!higher && treble_level > -8 && !init) {
+  } else if (!higher && treble_level > -8) {
     treble_level--;
   }
   write_to_decoder_function();
@@ -335,11 +297,6 @@ void write_to_decoder_function(void) {
   }
 }
 
-// void bassdecrease_isr(void) { xSemaphoreGiveFromISR(bassdecrease_semaphore, NULL); }
-// void bassincrease_isr(void) { xSemaphoreGiveFromISR(bassincrease_semaphore, NULL); }
-// void trebledecrease_isr(void) { xSemaphoreGiveFromISR(trebledecrease_semaphore, NULL); }
-// void trebleincrease_isr(void) { xSemaphoreGiveFromISR(trebleincrease_semaphore, NULL); }
-
 void bass_task(void *p) {
   while (true) {
     vTaskDelay(10);
@@ -348,7 +305,7 @@ void bass_task(void *p) {
       }
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      bass_function(false, false);
+      bass_function(false);
       // break;
       vTaskDelay(10);
     }
@@ -358,7 +315,7 @@ void bass_task(void *p) {
       }
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      bass_function(true, false);
+      bass_function(true);
       // break;
       vTaskDelay(10);
     }
@@ -374,7 +331,7 @@ void treble_task(void *p) {
       }
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      treble_function(false, false);
+      treble_function(false);
       // break;
       vTaskDelay(10);
     }
@@ -383,10 +340,9 @@ void treble_task(void *p) {
       }
       vTaskDelay(10);
       fprintf(stderr, "interrupt detected");
-      treble_function(true, false);
+      treble_function(true);
       // break;
       vTaskDelay(10);
     }
-    vTaskDelay(10);
   }
 }
